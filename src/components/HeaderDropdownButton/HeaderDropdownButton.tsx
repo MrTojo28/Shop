d75792f187811aa7be41a04e8263e9054c4221b1
MrTojo28 <MrTojo28@github.com>
2025-05-21 07:51:44 +0700

    фикс

--- src/components/HeaderDropdownButton/HeaderDropdownButton.tsx
+++ src/components/HeaderDropdownButton/HeaderDropdownButton.tsx
@@ -1,34 +1,60 @@
-import React, {useState} from 'react';
-import './HeaderDropdownButton.css';
-import { NavLink,  } from 'react-router';
+import React, { useState } from "react";
+import "./HeaderDropdownButton.css";
+import { NavLink } from "react-router";
 
 interface Props {
-  text?: string,
-  style?: React.CSSProperties,
-  onClick?: ()=>void,
-  items: {text: string, path: string, onClick?: () => void;}[],
-  path: string
+  text?: string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+  items: { text: string; path: string; onClick?: () => void }[];
+  path: string;
 }
 
-export function HeaderDropdownButton({ text, style, items, onClick, path }: Props) {
-  const [isMenuVisibe, setMenuVisible] = useState(false)
+export function HeaderDropdownButton({
+  text,
+  style,
+  items,
+  onClick,
+  path,
+}: Props) {
+  const [isMenuVisibe, setMenuVisible] = useState(false);
   return (
-    <div className={'header-dropdown-button-container'}>
-      <NavLink className={'header-dropdown-button'} to={path} style={style} onClick={()=>{onClick?.();}} onMouseOver={()=>setMenuVisible(!isMenuVisibe)}>
+    <div
+      className={"header-dropdown-button-container"}
+      onMouseLeave={() => {
+        setMenuVisible(false);
+      }}
+    >
+      <NavLink
+        className={"header-dropdown-button"}
+        to={path}
+        style={style}
+        onClick={() => {
+          onClick?.();
+        }}
+        onMouseOver={() => setMenuVisible(!isMenuVisibe)}
+      >
         {text}
       </NavLink>
-      {
-        isMenuVisibe &&
-        <div className='dropdown-menu'>
-          {
-            items.map((item,index)=>{
-              return <NavLink key={index} className={'dropdown-menu-item'} to={item.path} onClick={()=>{item.onClick?.();setMenuVisible(false)}}>
+      {isMenuVisibe && (
+        <div className="dropdown-menu">
+          {items.map((item, index) => {
+            return (
+              <NavLink
+                key={index}
+                className={"dropdown-menu-item"}
+                to={item.path}
+                onClick={() => {
+                  item.onClick?.();
+                  setMenuVisible(false);
+                }}
+              >
                 {item.text}
               </NavLink>
-            })
-          }
+            );
+          })}
         </div>
-      }
+      )}
     </div>
   );
 }
--- src/widgets/AboutUs/AboutUs.tsx
+++ src/widgets/AboutUs/AboutUs.tsx
@@ -1,104 +1,65 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import "./AboutUs.css";
 
-interface Props {
- 
-}
+interface Props {}
 
 export function AboutUs({}: Props) {
   return (
     <div className="about-us">
       <h1>О нас</h1>
       <p>
-    
-        О лаборатории
-
-Лаборатория сверхкритической флюидной экстракции (ЛСКФЭ) создана в 2023 году на базе ФГБОУ ВО «Кемеровский государственный университет» в рамках реализации ПСАЛ «Приоритет 2030», стратегический проект «Развитие собственных линий разработок и мелкосерийного производства инновационных продуктов».
-
-Основной деятельностью лаборатории является проведение прикладных исследований по разработке новых и оптимизации существующих протоколов извлечения биологически активных веществ из различных источников растительного, животного и микробиологического происхождения. Технология СКФЭ, использующая углекислый газ, является экологически безопасной и эффективной экстракционной техникой для твердых матриц, позволяющая в ряде случаев реализовывать принцип селективности при получении экстрактов, концентрированных по целевому веществу, либо группе таких веществ.
-
-Получаемые в результате реализации технологии СКФЭ продукты экстракции, содержащие протективные вещества, обладающих антиоксидантной, противовоспалительной, противоопухолевой и другими видами биологической активности могут служить основой для создания специализированных продуктов питания направленных на решение проблемы широкого распространения среди населения Российской Федерации социально значимых заболеваний, возникающих под воздействием вредных антропогенных факторов, включая, профессиональные и экологические.
-
-Научный коллектив обладает опытом проведения исследований по поиску источников и способов извлечения различных фитохимических веществ, а также разработки продуктов питания, включающих функциональные ингредиенты. В рамках деятельности лаборатории ведётся создание функциональных ингредиентов для пищевых продуктов – экстракты сухие и жидкие, водо- и жирорастворимые эмульсии экстрактов, шроты, а так же ведётся разработка функциональных продуктов питания. Продукция СКФЭ имеет декларацию о соответствии требованиям ЕАС и производится в коммерчески значимом количестве по заказу сторонних организаций.
-<br />
-Деятельность лаборатории:<br />
-• осуществляет реализацию научно-исследовательских и опытно-конструкторских работ по созданию новых технологий инновационной продукции по заказам сторонних организаций, а также методическое сопровождение проводимых научных исследований;
-<br />
-• обеспечивает научному коллективу возможность участия в крупных отечественных и международных проектах;
-<br />
-• создает необходимые условия для реализации учебного процесса, для проведения научно-исследовательской работы студентов направления «Технология продуктов питания из растительного сырья»;
-<br />
-• организует руководство научно-исследовательской работой студентов, магистрантов, аспирантов;
-<br />
-• проводит прикладные исследования в области функциональных ингредиентов и продуктов питания с привлечением студентов, магистрантов, аспирантов и сотрудников кафедры технологии продуктов питания из растительного сырья КемГУ.
-      </p>
-      <p>
-      
-      </p>
-   
-      <h1>Раздел 2</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-      Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-      Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <h1>Раздел 3</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-      Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-      Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <h1>Раздел 4</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-      Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      </p>
-      <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-      Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint beatae exercitationem doloribus commodi numquam? 
-        Deleniti, nemo ipsum. Sint ipsam quod incidunt illo officiis autem consectetur eveniet repellendus itaque soluta?
+        О лаборатории Лаборатория сверхкритической флюидной экстракции (ЛСКФЭ)
+        создана в 2023 году на базе ФГБОУ ВО «Кемеровский государственный
+        университет» в рамках реализации ПСАЛ «Приоритет 2030», стратегический
+        проект «Развитие собственных линий разработок и мелкосерийного
+        производства инновационных продуктов». Основной деятельностью
+        лаборатории является проведение прикладных исследований по разработке
+        новых и оптимизации существующих протоколов извлечения биологически
+        активных веществ из различных источников растительного, животного и
+        микробиологического происхождения. Технология СКФЭ, использующая
+        углекислый газ, является экологически безопасной и эффективной
+        экстракционной техникой для твердых матриц, позволяющая в ряде случаев
+        реализовывать принцип селективности при получении экстрактов,
+        концентрированных по целевому веществу, либо группе таких веществ.
+        Получаемые в результате реализации технологии СКФЭ продукты экстракции,
+        содержащие протективные вещества, обладающих антиоксидантной,
+        противовоспалительной, противоопухолевой и другими видами биологической
+        активности могут служить основой для создания специализированных
+        продуктов питания направленных на решение проблемы широкого
+        распространения среди населения Российской Федерации социально значимых
+        заболеваний, возникающих под воздействием вредных антропогенных
+        факторов, включая, профессиональные и экологические. Научный коллектив
+        обладает опытом проведения исследований по поиску источников и способов
+        извлечения различных фитохимических веществ, а также разработки
+        продуктов питания, включающих функциональные ингредиенты. В рамках
+        деятельности лаборатории ведётся создание функциональных ингредиентов
+        для пищевых продуктов – экстракты сухие и жидкие, водо- и
+        жирорастворимые эмульсии экстрактов, шроты, а так же ведётся разработка
+        функциональных продуктов питания. Продукция СКФЭ имеет декларацию о
+        соответствии требованиям ЕАС и производится в коммерчески значимом
+        количестве по заказу сторонних организаций.
+        <br />
+        Деятельность лаборатории:
+        <br />
+        • осуществляет реализацию научно-исследовательских и
+        опытно-конструкторских работ по созданию новых технологий инновационной
+        продукции по заказам сторонних организаций, а также методическое
+        сопровождение проводимых научных исследований;
+        <br />
+        • обеспечивает научному коллективу возможность участия в крупных
+        отечественных и международных проектах;
+        <br />
+        • создает необходимые условия для реализации учебного процесса, для
+        проведения научно-исследовательской работы студентов направления
+        «Технология продуктов питания из растительного сырья»;
+        <br />
+        • организует руководство научно-исследовательской работой студентов,
+        магистрантов, аспирантов;
+        <br />• проводит прикладные исследования в области функциональных
+        ингредиентов и продуктов питания с привлечением студентов, магистрантов,
+        аспирантов и сотрудников кафедры технологии продуктов питания из
+        растительного сырья КемГУ.
       </p>
     </div>
   );
